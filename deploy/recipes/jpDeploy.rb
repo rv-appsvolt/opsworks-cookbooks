b4b00045688e56f15f96a65ea472cdd85498833e
jp-appsvolt <49706008+jp-appsvolt@users.noreply.github.com>
2019-08-06 16:54:19 +0530

    Update jpDeploy.rb

--- deploy/recipes/jpDeploy.rb
+++ deploy/recipes/jpDeploy.rb
@@ -5,8 +5,8 @@ Chef::Log.info(node[:deploy])
 node[:deploy].each do |current_path, deploy|
   Chef::Log.info("npm start --prefix #{deploy[:current_path]}");
 #   Remove this tag comment in when we are going to user npm start.
-#   execute("sudo npm start --prefix #{deploy[:current_path]} &")
-  Chef::Log.info("sudo node #{deploy[:current_path]}/server.js &")
-  execute("sudo node #{deploy[:current_path]}/server.js &")
+  execute("sudo npm start --prefix #{deploy[:current_path]} &")
+#   Chef::Log.info("sudo node #{deploy[:current_path]}/server.js &")
+#   execute("sudo node #{deploy[:current_path]}/server.js &")
   Chef::Log.info("JP Custome Deployment Done")
 end

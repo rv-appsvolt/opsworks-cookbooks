6404c69f9f4153ea8f2fe0593fc9a51acdace152
jp-appsvolt <49706008+jp-appsvolt@users.noreply.github.com>
2019-08-06 15:56:34 +0530

    Update jpDeploy.rb

--- deploy/recipes/jpDeploy.rb
+++ deploy/recipes/jpDeploy.rb
@@ -1,7 +1,7 @@
 include_recipe 'deploy'
 Chef::Log.info("Ready to Deploy")
 Chef::Log.info(node[:deploy])
-Chef::Log.info(#{Application})
+Chef::Log.info("#{Application}")
 Chef::Log.info("Stage one")
 
 Chef::Log.info("JP Custome Deployment Server Start")

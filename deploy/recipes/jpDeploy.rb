1aa8be1bda9e8dacb15346998efc3caf90ca5c12
jp-appsvolt <49706008+jp-appsvolt@users.noreply.github.com>
2019-08-06 16:26:11 +0530

    Update jpDeploy.rb

--- deploy/recipes/jpDeploy.rb
+++ deploy/recipes/jpDeploy.rb
@@ -4,7 +4,9 @@ Chef::Log.info(node[:deploy])
 
 node[:deploy].each do |current_path, deploy|
   Chef::Log.info(deploy[:current_path])
-  Chef::Log.info("sudo node deploy[:current_path]/server.js &")
-#   execute("sudo node deploy[:current_path]/server.js &")
+  execute("cd sudo node deploy[:current_path]}")
+  execute("pwd")
+  Chef::Log.info("sudo node #{deploy[:current_path]}/server.js &")
+#   execute("sudo node #{deploy[:current_path]}/server.js &")
   Chef::Log.info("JP Custome Deployment Done")
 end

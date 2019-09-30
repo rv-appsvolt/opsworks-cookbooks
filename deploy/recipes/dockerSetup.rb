073291c587e66ea96000070bc8cf760d3bddf6fc
Rohit Vyas <mytrytest@gmail.com>
2019-09-30 15:41:12 +0530

    updates

--- deploy/recipes/dockerSetup.rb
+++ deploy/recipes/dockerSetup.rb
@@ -32,10 +32,9 @@ Chef::Log.info("Ready to Deploy")
 #execute("sudo apt-get install docker-ce")
 #execute("sudo docker -v")
 execute("sudo wget -qO- https://get.docker.com/ | sh")
-#execute("sudo curl -L 'https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose")
-#execute("sudo chmod +x /usr/local/bin/docker-compose")
-execute("pip install -U docker-compose")
-execute("chmod +x /usr/local/bin/docker-compose")
+execute("sudo curl -L 'https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose")
+execute("sudo chmod +x /usr/local/bin/docker-compose")
+
 
 
 

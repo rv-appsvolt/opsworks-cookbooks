268e0dfb01f2800f97954f40534b6393f8acdc57
Rohit Vyas <mytrytest@gmail.com>
2019-10-04 15:25:16 +0530

    updates

--- deploy/recipes/dockerEnv2.rb
+++ deploy/recipes/dockerEnv2.rb
@@ -35,10 +35,10 @@ node[:deploy].each do |current_path, deploy, environment_variables|
 	     end
 	     execute("cd #{deploy[:current_path]} && prisma deploy --force")
 	     Chef::Log.info("prisma deploy")
-	     execute("cd #{deploy[:current_path]} && sudo docker-compose stop")
-	     Chef::Log.info("docker stop")
-	     execute("cd #{deploy[:current_path]} && sudo docker-compose up -d")
-	     Chef::Log.info("docker up")
+	     #execute("cd #{deploy[:current_path]} && sudo docker-compose stop")
+	     #Chef::Log.info("docker stop")
+	     #execute("cd #{deploy[:current_path]} && sudo docker-compose up -d")
+	     #Chef::Log.info("docker up")
 	 else	
 		Chef::Log.info("else")     
     	 end

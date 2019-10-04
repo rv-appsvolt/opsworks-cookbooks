3f4af30f8ed9a1691aeef1881db246bbe99b4ea2
Rohit Vyas <mytrytest@gmail.com>
2019-10-04 19:07:24 +0530

    new file

---
+++ opsworks_nodejs/recipes/prismaInstall.rb
@@ -0,0 +1,6 @@
+include_recipe 'deploy'
+Chef::Log.info("prisma install")
+
+execute("sudo npm install -g prisma")
+execute(" sudo npm install -g dotenv")
+#execute("cd #{deploy[:current_path]} && sudo docker-compose up -d")
